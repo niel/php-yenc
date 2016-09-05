22cd166898ac98c6aa018069933ad9f1f27fe184
niel <niel.archer@gmail.com>
2016-09-05 01:46:53 +0100

    Adding package.sh script which makes use of FPM.

---
+++ package.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+conf=`php -i | grep -o "Scan this dir for additional .ini files => \S*" | cut -d\  -f9`
+major=`php -r "echo PHP_VERSION;" | cut -d. -f1`
+minor=`php -r "echo PHP_VERSION;" | cut -d. -f2`
+phpver="$major.$minor"
+
+fpm -s dir -t deb -n php$phpver-yenc -v 1.0.0 --depends "php > ${phpver}" --after-install=post-install.sh $(php-config --extension-dir)/yenc.so /etc/php/$phpver/mods-available/yenc.ini $conf/20-yenc.ini
