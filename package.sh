c17b2596633a758d94b5c3681df0b675ac5feb0e
niel <niel.archer@gmail.com>
2016-09-16 12:18:03 +0100

    Update packaging script for new version.

--- package.sh
+++ package.sh
@@ -5,4 +5,4 @@ major=`php -r "echo PHP_VERSION;" | cut -d. -f1`
 minor=`php -r "echo PHP_VERSION;" | cut -d. -f2`
 phpver="$major.$minor"
 
-fpm -s dir -t deb -n php$phpver-yenc -v 1.0.0 --depends "php > ${phpver}" --after-install=post-install.sh $(php-config --extension-dir)/yenc.so /etc/php/$phpver/mods-available/yenc.ini $conf/20-yenc.ini
+fpm -s dir -t deb -n php$phpver-yenc -v 1.1.1 --depends "php > ${phpver}" --after-install=post-install.sh $(php-config --extension-dir)/yenc.so /etc/php/$phpver/mods-available/yenc.ini $conf/20-yenc.ini
